1c73879122ecb3b68ce81ec6d8a06cb1cd011be9
unknown <joperigo@gmail.com>
2022-03-12 03:13:38 -0300

    Orders update

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -22,9 +22,9 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
+          <select name="status" class="status" @change="updatedBurger($event, burger.id)">
             <option value="">Selecione </option>
-            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
+            <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
           </select>
           <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
         </div>
@@ -81,6 +81,23 @@
         // msg
 
         this.getPedidos();
+      },
+      async updatedBurger(event, id) {
+
+        const option = event.target.value;
+
+        const dataJson = JSON.stringify({ status: option });
+
+        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: dataJson
+        });
+
+        const res = await req.json();
+
+        console.log(res);
+
       }
     },
     mounted() {

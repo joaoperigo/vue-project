d8a68a78d4890b941438eadbf1733129e5523526
unknown <joperigo@gmail.com>
2022-03-12 02:19:42 -0300

    Data rescue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{burger.id}}</div>
+        <div>{{burger.nome}}</div>
+        <div>{{burger.pao}}</div>
+        <div>{{burger.carne}}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>Tomate</li>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{opcional}}</li>
           </ul>
         </div>
         <div>
@@ -36,6 +35,31 @@
 <script>
   export default {
     name: "Dashboard",
+    data() {
+      return {
+        burgers: null,
+        burger_id: null,
+        status: []
+      }
+    },
+    methods: {
+      async getPedidos() {
+
+        const req = await fetch("http://localhost:3000/burgers")
+
+        const data = await req.json();
+
+        this.burgers = data;
+
+        console.log(this.burgers);
+
+        // rescue status
+
+      }
+    },
+    mounted() {
+      this.getPedidos();
+    }
   }
 </script>
 
